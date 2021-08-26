8ed7504975b09540a3446faecebcee8874aab938
dj <dj@ime.usp.br>
2021-08-26 12:29:44 -0300

    Separando componente Agua

--- src/index.js
+++ src/index.js
@@ -1,51 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Route from "react-router-dom";
-
-class Agua extends React.Component {
-    state = { estado: 'liquido', temperatura: '29'};
-
-    constructor() {
-        super();
-        this.verEstado = this.verEstado.bind(this);
-        this.estadoGelado = this.estadoGelado.bind(this);
-        this.estadoLiquido = this.estadoLiquido.bind(this);
-        this.estadoGasoso = this.estadoGasoso.bind(this);
-    }
-
-    verEstado(event) {
-        console.log("Estado atual: " + this.state.estado);
-    }
-
-    estadoGelado(event) {
-        this.setState( { estado: 'gelado', temperatura: '-273'} );
-    }
-
-    estadoLiquido(event) {
-        this.setState( { estado: 'liquido', temperatura: '29'} );
-    }
-
-    estadoGasoso(event) {
-        this.setState( { estado: 'gasoso', temperatura: '150'} );
-    }
-
-    render() {
-        return (
-            <div>
-                <p>Propriedades da água:</p>
-                <p>Fusão: {this.props.fusao} graus</p>
-                <p>Ebulição: {this.props.ebulicao} graus</p>
-                <p>Condição: {this.props.condicao}</p>
-
-                <button onClick={ this.verEstado } > Ver estado</button>
-                
-                <button onClick={ this.estadoGelado } > Gelado </button>
-                <button onClick={ this.estadoLiquido } > Liquido </button>
-                <button onClick={ this.estadoGasoso } > Gasoso </button>
-            </div>
-        )
-    }
-}
+import Agua from "./Agua";
 
 ReactDOM.render(
     <Agua fusao={0} ebulicao={100} condicao="CNTP" />,
