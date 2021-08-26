9f3ba95cba26e08b670f8f3b46a2b0c3c28817c1
dj <dj@ime.usp.br>
2021-08-26 11:29:16 -0300

    Componente Água criado!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Agua extends React.Component {
+    state = { estado: 'liquido', temperatura: '29'};
+
+    constructor() {
+        super();
+        this.verEstado = this.verEstado.bind(this);
+        this.estadoGelado = this.estadoGelado.bind(this);
+        this.estadoLiquido = this.estadoLiquido.bind(this);
+        this.estadoGasoso = this.estadoGasoso.bind(this);
+    }
+
+    verEstado(event) {
+        console.log("Estado atual: " + this.state.estado);
+    }
+
+    estadoGelado(event) {
+        this.setState( { estado: 'gelado', temperatura: '-273'} );
+    }
+
+    estadoLiquido(event) {
+        this.setState( { estado: 'liquido', temperatura: '29'} );
+    }
+
+    estadoGasoso(event) {
+        this.setState( { estado: 'gasoso', temperatura: '150'} );
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Propriedades da água:</p>
+                <p>Fusão: {this.props.fusao} graus</p>
+                <p>Ebulição: {this.props.ebulicao} graus</p>
+                <p>Condição: {this.props.condicao}</p>
+
+                <button onClick={ this.verEstado } > Ver estado</button>
+                
+                <button onClick={ this.estadoGelado } > Gelado </button>
+                <button onClick={ this.estadoLiquido } > Liquido </button>
+                <button onClick={ this.estadoGasoso } > Gasoso </button>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Agua fusao={0} ebulicao={100} condicao="CNTP" />,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
